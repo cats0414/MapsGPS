44dd065d1c4d90efd54ff055335849ecb838d0f7
alfredohugueth <alfredohugueth99@gmail.com>
2020-09-15 21:38:51 -0500

    Base de datos

---
+++ Pagina web/public/baseDat.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE Employees;
+USE Employees;
+CREATE TABLE usuarios(
+    id int(11) NOT NULL, 
+    latitud float(16) NOT NULL,
+    longitud float(16) NOT NULL,
+    tiempo VARCHAR(30) NOT NULL
+);
---
+++ Pagina web/public/database.js
@@ -0,0 +1,22 @@
+import mysql from 'mysql';
+const {promisify} = require('util');
+database = 'Employees'
+const pool = mysql.createPool(database);
+pool.getConnection((err, connection) => {
+    if (err){
+       if (err.code === 'PROTOCOL_CONNECTION_LOST'){
+           console.error('CONEXION CON BASE DE DATOS PERDIDA');
+       }
+       if (err.code === 'ER_CON_COUNT_ERROR'){
+            console.error('CONEXIONES OCUPADAS');
+       }
+    if (err.code === 'ECONNREFUSED'){
+        console.error('CONEXION RECHAZADA');
+    }
+    }
+    if (connection) connection.release();
+    console.log('Base de datos conectada')
+    return;
+});
+pool.query = promisify(pool.query);
+module.exports = pool;
