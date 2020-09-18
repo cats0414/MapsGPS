9a8688e5059ffcfa072714b9dad903308b93ab3b
cats0414 <69682554+cats0414@users.noreply.github.com>
2020-09-18 14:23:25 -0500

    Update server.js

--- Pagina web/server.js
+++ Pagina web/server.js
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-const express = require('express');
-const app = express();
-const mysql = require('mysql');
-
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
-=======
->>>>>>> 9d1b8a211999e297efb14c22f14cbe6f270a515b
-=======
 const express = require('express');
 const app = express();
+var mensaje= 'hola';
 const mysql = require('mysql');
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 require('dotenv').config()
-
-
 // Creamos credenciales para ingresar a la base de datos
 const database = mysql.createConnection({
 		host: process.env.db_host, user: process.env.db_user , password: process.env.db_pass, database: process.env.db_data , port: process.env.db_port
@@ -41,52 +27,14 @@ database.connect((err,connection) =>{
 });
 
 
+
 app.set('view engine', 'ejs');
-<<<<<<< HEAD
->>>>>>> origin/alfredodev
-=======
->>>>>>> alfredodev
-=======
-var mensaje = 'Hola';
->>>>>>> 28027d24434434ced13574b9a5965cb50c1e7cc4
 
-const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
->>>>>>> 9d1b8a211999e297efb14c22f14cbe6f270a515b
-
-
-
-// Creamos credenciales para ingresar a la base de datos
-const database = mysql.createConnection({
-        host: 'db-cats.ckplc44wafdp.us-east-1.rds.amazonaws.com', user: 'admin' , password: '01201404', database: 'db-cats', port: 3306
-});
-// conectamos con la base de datos
-database.connect((err) =>{
-    if (err){
-        if (err.code === 'PROTOCOL_CONNECTION_LOST'){
-            console.error('CONEXION CON BASE DE DATOS PERDIDA');
-        }
-        if (err.code === 'ER_CON_COUNT_ERROR'){
-             console.error('CONEXIONES OCUPADAS');
-        }
-     if (err.code === 'ECONNREFUSED'){
-         console.error('CONEXION RECHAZADA');
-     }
-     }
-     if (connection) connection.release();
-     console.log('Base de datos conectada')
+server.on('error', (err) => {
+        console.log(`server error:\n${err.stack}`);
+        server.close();
 });
 
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-app.set('view engine', 'ejs');
-
-
-server.on('error', (err) => {
-    console.log(`server error:\n${err.stack}`);
-    server.close();
-=======
 server.on('message', (msg, rinfo) => {
     console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
     mensaje = msg;
@@ -95,56 +43,30 @@ server.on('message', (msg, rinfo) => {
     let sql = 'INSERT INTO usuarios SET ?';
     let query = database.query(sql, msg, (err, result) => {
     if (err){
-	console.trace('error=' + err.message);
+        console.trace('error=' + err.message);
 };
 });
-<<<<<<< HEAD
->>>>>>> origin/alfredodev
-=======
->>>>>>> alfredodev
->>>>>>> 9d1b8a211999e297efb14c22f14cbe6f270a515b
 });
 
-
-server.on('message', (msg, rinfo) => {
-    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-    mensaje = msg;
-<<<<<<< HEAD
-	msg = msg.toString().split(",")
-	msg = {latitud: msg[0], longitud: msg[1], fecha:msg[2]}
-	let sql = 'INSERT INTO datos SET ?';
-	let query = database.query(sql, msg, (err, result) => {
-	if (err) throw err;
+server.on('listening', () => {
+        const address = server.address();
+        console.log(`server listening ${address.address}:${address.port}`);
 });
-=======
->>>>>>> 9d1b8a211999e297efb14c22f14cbe6f270a515b
-});
-
-
-server.on('listening', () => {
-    const address = server.address();
-    console.log(`server listening ${address.address}:${address.port}`);
-});
-
 
 server.bind(3659);
 
-
-app.get('/', function (req, res) {
-    res.render('index', {
-        msg: mensaje,
-    });
+app.get('/', function (req, res) {
+        res.render('index', {
+                msg: mensaje,
+        });
 });
-
 
 app.use(express.static('public'));
 
-
-app.get('/ruta', function (req, res) {
-    res.json({ msg: mensaje });
+app.get('/ruta', function (req, res) {
+        res.json({ msg: mensaje });
 });
 
-
-app.listen('40000', function () {
-    console.log('Todo en orden');
+app.listen('40000', function () {
+        console.log('Todo en orden');
 });
