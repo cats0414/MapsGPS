a21eb18fa5997da5072dd0e48e2838495710391c
Ubuntu <ubuntu@ip-172-31-46-37.ec2.internal>
2020-09-24 01:07:03 +0000

    PolilineaCompleta

--- Pagina web/public/codigomaps.js
+++ Pagina web/public/codigomaps.js
@@ -5,39 +5,9 @@ var lng
 
 
 function initMap(){
-	fetch('/ruta')
-		.then((response) => response.json())
-		.then((json) => {
-		console.log(json);
-		var str = '';
-		for (var i = 0; i < json.msg.data.length; ++i) {
-			str += String.fromCharCode(json.msg.data[i]);
-		}
+	map = new google.maps.Map(document.getElementById('map'),{
+	zoom: 10,
+	center: {lat:11.003  , lng:-74.82 }
+	});
 	
-			datos = str.split(',');
-			latitud = Number.parseFloat(datos[0]);
-			longitud = Number.parseFloat(datos[1]);
-			fecha = datos[2];
-			var lta = latitud;
-			var	lng = longitud;
-			if (isNaN(lta)||isNaN(lng)){
-				ltan=11.003;
-				lonn=-74.82;
-				}else{
-				ltan=lta;
-				lonn=lng;
-				}
-				coordi = {lat:ltan,lng:lonn}
-				map = new google.maps.Map(document.getElementById('map'),{
-				zoom: 15,
-				center: coordi
-				});
-				marcador = new google.maps.Marker({
-					position: coordi ,
-					map: map
-				});
-				setTimeout(initMap, 15000);
-				});	
-}
-initMap();
-	
+}	
