403cd194cb3e9dec60fa0c3bfc5a4ccd7c8c445f
cats0414 <catous@uninorte.edu.co>
2020-09-26 15:37:35 -0500

    PwebUpdated

--- Pagina web/public/codigomaps.js
+++ Pagina web/public/codigomaps.js
@@ -1,94 +1,13 @@
 let map;
-<<<<<<< .merge_file_Q7G4hA
-var coordi;
-function update() {
-	fetch('/ruta')
-		.then((response) => response.json())
-		.then((json) => {
-			console.log(json);
-			var str = '';
-			for (var i = 0; i < json.msg.data.length; ++i) {
-				str += String.fromCharCode(json.msg.data[i]);
-			}
-
-			datos = str.split(',');
-			latitud = Number.parseFloat(datos[0]);
-			longitud = Number.parseFloat(datos[1]);
-			fecha = datos[2];
-			var lta = latitud;
-			var	lng = longitud;
-			if (isNaN(lta)||isNaN(lng)){
-				ltan=11.003;
-				lonn=-74.82;
-			}else{
-				ltan=lta;
-				lonn=lng;
-			}
-			coordi = {lat:ltan,lng:lonn}
-<<<<<<< HEAD
-			setTimeout(update, 1000);
-=======
-			setTimeout(update, 4000);
-<<<<<<< HEAD
->>>>>>> origin/alfredodev
-=======
->>>>>>> alfredodev
->>>>>>> 9d1b8a211999e297efb14c22f14cbe6f270a515b
-			return coordi;
-		});
-}
-update();
-=======
 var coordi={lat:11.003  , lng:-74.82 };
 var lta
 var lng
 
 
->>>>>>> .merge_file_7m8F8z
 function initMap(){
 	map = new google.maps.Map(document.getElementById('map'),{
 	zoom: 10,
 	center: {lat:11.003  , lng:-74.82 }
 	});
-<<<<<<< .merge_file_Q7G4hA
-	function actMarc(){
-	marcador = new google.maps.Marker({
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-		position: coordi ,
-		map: map
-	});
-<<<<<<< HEAD
-=======
->>>>>>> 9d1b8a211999e297efb14c22f14cbe6f270a515b
-				position: coordi ,
-				map: map
-			});
->>>>>>> 28027d24434434ced13574b9a5965cb50c1e7cc4
-	setTimeout(actMarc, 1000);
-<<<<<<< HEAD
-	}
-	actMarc();
-}
-=======
-		position: coordi ,
-		map: map
-	});
-=======
-=======
->>>>>>> 9d1b8a211999e297efb14c22f14cbe6f270a515b
-	setTimeout(actMarc, 5000);
->>>>>>> alfredodev
-	}
-	actMarc();
-<<<<<<< HEAD
-}
->>>>>>> origin/alfredodev
-=======
-}
->>>>>>> 9d1b8a211999e297efb14c22f14cbe6f270a515b
-=======
 	
 }	
->>>>>>> .merge_file_7m8F8z
--- Pagina web/server.js
+++ Pagina web/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
-var mensaje= 'hola';
 const mysql = require('mysql');
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
-require('dotenv').config()
-<<<<<<< HEAD
-var mensaje='Hola'
-
-//Creamos credenciales para ingresar a la base de datos
+const path = require('path');
+var mensaje = 'Hola';
+app.set('views', path.join(__dirname, 'views'));
+app.set('imagenes', path.join(__dirname, '/public/imagenes'));
+app.set('calendar', path.join(__dirname, '/public/calendar'));
+require('dotenv').config();
+// Creamos credenciales para ingresar a la base de datos
 const database = mysql.createConnection({
-		host: process.env.db_host, user: process.env.db_user , password: process.env.db_pass, database: process.env.db_data , port: process.env.db_port
+		host: process.env.db_host, user: process.env.db_user , password: process.env.db_pass, database: process.env.db_data, port: process.env.db_port
 });
 // conectamos con la base de datos
 database.connect((err,connection) =>{
@@ -30,12 +31,11 @@ database.connect((err,connection) =>{
 });
 
 
-
 app.set('view engine', 'ejs');
 
 server.on('error', (err) => {
-        console.log(`server error:\n${err.stack}`);
-        server.close();
+	console.log(`server error:\n${err.stack}`);
+	server.close();
 });
 
 server.on('message', (msg, rinfo) => {
@@ -45,34 +45,36 @@ server.on('message', (msg, rinfo) => {
     lati = parseFloat(msg[0]).toFixed(4);
     longi = parseFloat(msg[1]).toFixed(4);
     msg = {id : "1" ,latitud: lati, longitud: longi, tiempo: msg[2]}
-    msg2 = {id: "1" ,latitud: parseFloat(msg[0]), longitud: parseFloat(msg[1]),tiempo: msg[2]} 
-    let sql = 'INSERT INTO usuarios2 SET ?';
+    msg = {id: "1" ,latitud: msg[0], longitud: msg[1],tiempo: msg[2]} 
+    let sql = 'INSERT INTO usuarios SET ?';
     let query = database.query(sql, msg, (err, result) => {
     if (err){
-        console.trace('error=' + err.message);
+	console.trace('error=' + err.message);
 };
 });
 });
 
 server.on('listening', () => {
-        const address = server.address();
-        console.log(`server listening ${address.address}:${address.port}`);
+	const address = server.address();
+	console.log(`server listening ${address.address}:${address.port}`);
 });
 
 server.bind(3659);
 
 app.get('/', function (req, res) {
-        res.render('index', {
-                msg: mensaje,
-        });
+	res.render('index', {
+		msg: mensaje,
+	});
 });
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
+
+
 
 app.get('/ruta', function (req, res) {
-        res.json({ msg: mensaje });
+	res.json({ msg: mensaje });
 });
 
 app.listen('40000', function () {
-        console.log('Todo en orden');
+	console.log('Todo en orden');
 });
