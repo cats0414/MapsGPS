1ea07e028b7b3f5382bdfc98c918569c6006c3f0
alfredohugueth <alfredohugueth99@gmail.com>
2020-11-04 17:22:38 -0500

    Mas detalles

--- Pagina web/public/Consulta.js
+++ Pagina web/public/Consulta.js
@@ -145,7 +145,7 @@
 				tolerancia = 0.02;
 				//distanciaMaxi = Math.acos(Math.sin(latpri)*)
 				if (obj.lat >= latit && obj.lng >= lngit && obj.lat <= latit2 && obj.lng <= lngit2) {
-					pruebs = Math.acos(Math.sin(deg2rad((latit+latit2)/2)*Math.sin(deg2rad(obj.lat)+Math.cos(deg2rad((latit+latit2)/2))*Math.cos(deg2rad(obj.lat))*Math.cos(deg2rad(obj.lng)-deg2rad((lngit+lngit2)/2)))));
+					pruebs = Math.acos(Math.sin(deg2rad((latit+latit2)/2))*Math.sin(deg2rad(obj.lat))+Math.cos(deg2rad((latit+latit2)/2))*Math.cos(deg2rad(obj.lat))*Math.cos(deg2rad(obj.lng)-deg2rad((lngit+lngit2)/2)));
 					console.log(pruebs);
 				  if(Math.acos(Math.sin(deg2rad((latit+latit2)/2)*Math.sin(deg2rad(obj.lat)+Math.cos(deg2rad((latit+latit2)/2))*Math.cos(deg2rad(obj.lat))*Math.cos(deg2rad(obj.lng)-deg2rad((lngit+lngit2)/2)))))<0.02){
 					return true;
