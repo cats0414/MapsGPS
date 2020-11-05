355f0eacdee4cbaf0d309252c6b30b50fb58bffb
cats <catous@uninorte.edu.co>
2020-11-05 00:34:59 -0500

    App con sensor implementado

--- app/src/main/java/com/example/hmrtgps/Bluetooth.java
+++ app/src/main/java/com/example/hmrtgps/Bluetooth.java
@@ -9,20 +9,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.hp.bluetoothjhr.BluetoothJhr;
 
 public class Bluetooth extends AppCompatActivity {
-    ListView Lista;
+    ListView ListaDispositivos;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
-        Lista=(ListView)findViewById(R.id.disp);
-        final BluetoothJhr bluetoothJhr= new BluetoothJhr(this,Lista);
+        ListaDispositivos = (ListView) findViewById(R.id.ListaDispositivos);
+        final BluetoothJhr bluetoothJhr = new BluetoothJhr(this, ListaDispositivos);
         bluetoothJhr.EncenderBluetooth();
-        Lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        ListaDispositivos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            bluetoothJhr.Disp_Seleccionado(view,position,MainActivity.class);
+                bluetoothJhr.Disp_Seleccionado(view,position,MainActivity.class);
             }
         });
-    }
-    public void setContentView(int activity2) {
     }
 }
