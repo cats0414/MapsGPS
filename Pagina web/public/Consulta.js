61cea031df2b97782831b3dc20914b47bc62229d
alfredohugueth <alfredohugueth99@gmail.com>
2020-11-11 18:41:36 -0500

    Mas mejoras filtro

--- Pagina web/public/Consulta.js
+++ Pagina web/public/Consulta.js
@@ -15,6 +15,7 @@
 		let contadorGeneral = 0;
 		let linea2 = [];
 		let linea3 = [];
+		var tiempoCorreg = [];
 		var TiemposMostrados;
 		const Radio = 6371; //Radio de la tierra
 			const formLogin = document.querySelector('#formulario');
@@ -122,13 +123,13 @@
 				coordenadas = event.latLng;
 				latpri = coordenadas.lat();
 				lngpri = coordenadas.lng();	
-				latit2 = parseFloat(coordenadas.lat()) + rad2deg(0.003/Radio);
+				latit2 = parseFloat(coordenadas.lat()) + rad2deg(0.005/Radio);
 				
-				lngit2 = parseFloat(coordenadas.lng()) + (rad2deg(Math.asin(0.003/Radio)))/(Math.cos(deg2rad(coordenadas.lat())));
+				lngit2 = parseFloat(coordenadas.lng()) + (rad2deg(Math.asin(0.005/Radio)))/(Math.cos(deg2rad(coordenadas.lat())));
 				
-				latit = parseFloat(coordenadas.lat()) - rad2deg(0.003/Radio);
+				latit = parseFloat(coordenadas.lat()) - rad2deg(0.005/Radio);
 				
-				lngit = parseFloat(coordenadas.lng()) - (rad2deg(Math.asin(0.003/Radio)))/(Math.cos(deg2rad(coordenadas.lat())));
+				lngit = parseFloat(coordenadas.lng()) - (rad2deg(Math.asin(0.005/Radio)))/(Math.cos(deg2rad(coordenadas.lat())));
 				
 				ValoresConsul = valores.filter(filtrarPorPosicion);
 				if(ValoresConsul.length == 0){
@@ -165,7 +166,7 @@
 			}
 			}
 			function filtrarPorPosicion(obj) {
-				tolerancia = 0.008;
+				tolerancia = 0.005;
 				//distanciaMaxi = Math.acos(Math.sin(latpri)*)
 				if (obj.lat >= latit && obj.lng >= lngit && obj.lat <= latit2 && obj.lng <= lngit2) {
 					pruebs = Math.acos(Math.sin(deg2rad((latit+latit2)/2))*Math.sin(deg2rad(obj.lat))+Math.cos(deg2rad((latit+latit2)/2))*Math.cos(deg2rad(obj.lat))*Math.cos(deg2rad(obj.lng)-deg2rad((lngit+lngit2)/2)));
